fda39db59aa9519a1211acdd277bee05df279e9b
teiatan <92535482+teiatan@users.noreply.github.com>
2023-06-21 11:37:03 +0300

    made last node handle transparent

--- src/components/Node/Node.jsx
+++ src/components/Node/Node.jsx
@@ -1,14 +1,30 @@
-import { Handle, Position } from 'reactflow';
+import { Handle, Position, useNodeId } from 'reactflow';
 import { CustomContainer } from '../CustomContainer/CustomContainer';
 import 'reactflow/dist/style.css';
+import { useSelector } from 'react-redux';
+import { selectNodes } from 'redux/selectors';
 
 export const Node = ({ data }) => {
+  const nodeId = useNodeId();
+  const nodes = useSelector(selectNodes);
+  let sourceHandleBackgroundColor = {backgroundColor: 'grey'};
+  if(nodes[nodes.length -1].id === nodeId) {
+    sourceHandleBackgroundColor = {backgroundColor: 'transparent'};
+  }
 
-    return (
-      <>
-        <Handle type="target" position={Position.Top} style={{backgroundColor: 'transparent', width: '1px', border:'none'}}/>
-        <CustomContainer />
-        <Handle type="source" position={Position.Bottom}  style={{backgroundColor: 'grey', width: '1px', border:'none', zIndex:-1}}/>
-      </>
-    );
+  return (
+    <>
+      <Handle 
+        type="target" 
+        position={Position.Top} 
+        style={{backgroundColor: 'transparent', width: '1px', border:'none'}}
+      />
+      <CustomContainer />
+      <Handle 
+        type="source" 
+        position={Position.Bottom}  
+        style={{width: '1px', border:'none', zIndex:-1, ...sourceHandleBackgroundColor}}
+      />
+    </>
+  );
   }
